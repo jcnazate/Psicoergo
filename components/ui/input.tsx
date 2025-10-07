75c0b9bcacb85c5ad7139dea98045dfe519499e6
Jcnazate11 <jcnazate@espe.edu.ec>
2025-10-06 21:58:01 -0500

    Cambios Errores

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,28 +1,23 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = "text", ...props }, ref) => {
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
     return (
       <input
-        type={type}
+        ref={ref}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background",
-          "px-3 py-2 text-sm ring-offset-background",
-          "placeholder:text-muted-foreground",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2",
+          "text-sm ring-offset-background placeholder:text-muted-foreground",
           "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
           "disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
-
-export { Input }
+);
+Input.displayName = "Input";
